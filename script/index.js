2e80002190a5df402d84099f614212061eb9377f
keilo-bravo <kesbto@gmail.com>
2022-08-18 03:21:55 -0300

    difficulty selector, difficulty madia in progress

--- script/index.js
+++ script/index.js
@@ -3,8 +3,8 @@ const tablero = {
     "O":[]
 }
 const dificultad = {
-    "vs_player":false,
-    "facil":true,
+    "vs_player":true,
+    "facil":false,
     "medio":false,
     "dificil":false
 }
@@ -17,6 +17,15 @@ const Pts = {
     "X":0,
     "E":0,
     "O":0
+}
+function select() {
+    const opcion = document.getElementById("select").value
+    for (let i in dificultad) {
+        dificultad[i]=false
+    }
+    dificultad[opcion] = true
+    Player.turn='X'
+    newGame()
 }
 function Accion() {
     if (dificultad.vs_player) {
@@ -69,8 +78,29 @@ function comFacil() {
         verify()
     }
 }
-function comMedio(writeArea, jugada) {
-
+function comMedio() {
+    const jugadas = tablero.X.concat(tablero.O)
+    const jugadasProx = tableroVacio(jugadas)
+    if (tablero[Player.turn].length===0) {
+        let x=Math.floor(Math.random()*(3-0)+0)
+        let y=Math.floor(Math.random()*(3-0)+0)
+        if (jugadas.includes(`P${y}-${x}`)) {
+            comMedio()
+        }   else{
+            document.getElementById(`P${y}-${x}`).firstElementChild.textContent=Player.turn
+            tablero[Player.turn].push(`P${y}-${x}`)
+            verify()
+        }
+    }   else{
+        console.log(jugadasProx, 'jugadas pendientes');
+        // verificar jugadas del contrincante
+        // si tiene 2 en fila bolquear
+        // sino tratar de poner 3 en fila
+        let comJ = block(jugadasProx,0)
+        document.getElementById(`${comJ}`).firstElementChild.textContent=Player.turn
+        tablero[Player.turn].push(`${comJ}`)
+        verify()
+    }
 }
 function comDificil(writeArea, jugada) {
 
@@ -153,6 +183,92 @@ function tableroDisable() {
         }
     }
 }
+function tableroVacio(tablero) { 
+    const tableroV = []
+    for (let i = 0; i < 3; i++) {        
+        for (let j = 0; j < 3; j++) {
+            if (!tablero.includes(`P${i}-${j}`)){
+                tableroV.push(`P${i}-${j}`)
+            }
+        }
+    }
+    return tableroV
+}
+function block(tVacio,n) {
+    console.log('ejecuto????');
+    let aux = []
+    let aux2 = []
+    let play = tablero[Player.player].sort()
+    let tab = tablero[Player.com].concat(tablero[Player.player]).sort()
+    switch (n) {
+        case 0:
+        case 1:
+        case 2: 
+        console.log(n, '= n');
+            for (let i = 0; i < 3; i++) {
+                aux.push(`P${n}-${i}`)
+            }
+            for (let i = 0; i < aux.length; i++) {
+                if (!play.includes(aux[i])&&tab.includes(aux[i])) {
+                    aux2.push(false)//aux[i]
+                }
+                if (!play.includes(aux[i])&&!tab.includes(aux[i])) {
+                    aux2.push(aux[i])//aux[i]
+                }
+            }
+            aux2 = aux2.sort()
+            console.log(aux2);
+            break;
+        case 3:
+        case 4:
+        case 5: 
+        console.log(n, '= n');
+            for (let i = 0; i < 3; i++) {
+                aux.push(`P${i}-${n-3}`)
+            }
+            for (let i = 0; i < aux.length; i++) {
+                if (!play.includes(aux[i])&&tab.includes(aux[i])) {
+                    aux2.push(false)//aux[i]
+                }
+                if (!play.includes(aux[i])&&!tab.includes(aux[i])) {
+                    aux2.push(aux[i])//aux[i]
+                }
+            }
+            aux2 = aux2.sort()
+            console.log(aux2);
+            break;
+        case 6:
+        console.log(n, '= n');
+            let j = 2
+            for (let i = 0; i < 3; i++) {
+                if (!play.includes(`P${j}-${i}`)) {
+                    aux2.push(`P${j}-${i}`)
+                }
+                --j
+            }
+            break;
+        case 7:
+        console.log(n, '= n');
+            //let j = 2
+            for (let i = 0; i < 3; i++) {
+                if (!play.includes(`P${j}-${i}`)) {
+                    aux2.push(`P${j}-${i}`)
+                }
+                --j
+            }
+            break;
+        default:
+            //tratar de poner 3 en fila
+            
+            break;
+    }
+    console.log(n, 'no sw');
+    if ((aux2.length>1&&tVacio.length>1)||aux2.includes(false)) {
+        console.log('ejecuta el if???');
+        return block(tVacio,n+1)       
+    }
+    return aux2[0]
+}
 function newGame() {
     const tabla = []
     for (let i = 0; i < 3; i++) {        
@@ -166,4 +282,5 @@ function newGame() {
         tablero.O.length=0
         tablero.X.length=0
     })
+    Accion()
 }
